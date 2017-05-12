b29166c7fbaa35bfa961029655d1f6542888b6f0
simon.vezey <simon.vezey@activus.co.uk>
2017-05-12 12:54:45 +0100

    starting on front end

--- Snapshotter/Snapshotter.cs
+++ Snapshotter/Snapshotter.cs
@@ -1,7 +1,52 @@
-﻿namespace Snapshotter
+﻿using System.Collections.Generic;
+using System.Linq;
+using Snapshotter.Database;
+
+namespace Snapshotter
 {
     public class Snapshotter
     {
-        
+        private readonly DatabaseInfoRepo _repo;
+
+        public Snapshotter()
+        {
+            _repo = new DatabaseInfoRepo();
+        }
+
+        public IEnumerable<Domain.Database> GetDatabases()
+        {
+            return _repo.GetDatabases().Select(x => new Domain.Database(_repo) { Id = x.database_id, Name = x.name });
+        }
+    }
+    
+    namespace Domain
+    {
+        public class Database
+        {
+            private readonly DatabaseInfoRepo _repo;
+
+            internal Database(DatabaseInfoRepo repo)
+            {
+                _repo = repo;
+            }
+
+            public int Id { get; internal set; }
+
+            public string Name { get; internal set; }
+
+            public List<Snapshot> GetSnapshots()
+            {
+                return _repo.GetSnapshots(Id).Select(x => new Snapshot { Name = x.name, Id = x.database_id}).ToList();
+            }
+        }
+    }
+}
+
+namespace Snapshotter.Domain
+{
+    public class Snapshot
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
--- SqlSnapshot/MainWindow.xaml.cs
+++ SqlSnapshot/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Snapshotter.Domain;
 
 namespace SqlSnapshot
 {
@@ -20,9 +11,43 @@ namespace SqlSnapshot
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainWindowViewModel _viewModel = new MainWindowViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = _viewModel;
+            var snapshotter = new Snapshotter.Snapshotter();
+
+            _viewModel.Databases = snapshotter.GetDatabases().Select(x => new DatabaseViewModel {Database = x, Name = x.Name, Id = x.Id}).ToList();
+            _viewModel.Names = _viewModel.Databases.Select(x => x.Name).ToList();
         }
     }
+}
+
+public class MainWindowViewModel
+{
+    public List<DatabaseViewModel> Databases { get; set; }
+    public List<string> Names { get; set; }
+
+    public DatabaseViewModel SelectedDatabase { get; set; }
+
+    public SnapShotViewModel SelectedSnapshot { get; set; }
+}
+
+public class DatabaseViewModel
+{
+    public Database Database { get; set; }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public List<SnapShotViewModel> SnapShots => Database.GetSnapshots().Select(x => new SnapShotViewModel {Name = x.Name}).ToList();
+}
+
+public class SnapShotViewModel
+{
+    public string Name { get; set; }
 }
